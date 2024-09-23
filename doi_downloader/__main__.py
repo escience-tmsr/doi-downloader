1a438048404be5fa1f60fca182dc396aaae59790
recap <recap@users.noreply.github.com>
2024-09-23 15:32:13 +0000

    ✅ Ready to clone and code.

---
+++ doi_downloader/__main__.py
@@ -0,0 +1,6 @@
+"""Entry point for doi_downloader."""
+
+from doi_downloader.cli import main  # pragma: no cover
+
+if __name__ == "__main__":  # pragma: no cover
+    main()
--- doi_downloader/cli.py
+++ doi_downloader/cli.py
@@ -1,4 +1,4 @@
-"""CLI interface for project_name project.
+"""CLI interface for doi_downloader project.
 
 Be creative! do whatever you want!
 
@@ -12,7 +12,7 @@ Be creative! do whatever you want!
 def main():  # pragma: no cover
     """
     The main function executes on commands:
-    `python -m project_name` and `$ project_name `.
+    `python -m doi_downloader` and `$ doi_downloader `.
 
     This is your program's entry point.
 
