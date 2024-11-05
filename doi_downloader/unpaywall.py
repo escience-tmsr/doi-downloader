5ec1cb8be408c61b3c63cbba3a4b41156916b116
Reggie Cushing <r.cushing@esciencecenter.nl>
2024-11-05 16:29:21 +0100

    update unit test for unpaywall

--- doi_downloader/unpaywall.py
+++ doi_downloader/unpaywall.py
@@ -1,31 +1,29 @@
 import requests
 
-
 # Read API keys and other sensitive data from environment variables
 UNPAYWALL_EMAIL = None
 UNPAYWALL_API_URL = "https://api.unpaywall.org/v2/{doi}?email={email}"
+headers = {
+    'User-Agent': "Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/81.0"
+}
 
-# Check if necessary variables are loaded
-# if not UNPAYWALL_EMAIL:
-#     raise EnvironmentError("Please make sure UNPAYWALL_EMAIL are set in the .env file.")
-
+# Function to set the email for the Unpaywall API
 def set_email(email):
     global UNPAYWALL_EMAIL
     UNPAYWALL_EMAIL = email
 
-
+# Function to get the URL of the PDF from the DOI
 def get_url(doi):
     if not UNPAYWALL_EMAIL:
         raise EnvironmentError("Please make sure email is set using set_email().")
 
     # Make the request to the Unpaywall API
-    response = requests.get(UNPAYWALL_API_URL.format(doi=doi, email=UNPAYWALL_EMAIL))
+    response = requests.get(UNPAYWALL_API_URL.format(doi=doi, email=UNPAYWALL_EMAIL), headers=headers)
 
     if response.status_code == 200:
         data = response.json()
         if "best_oa_location" in data and data["best_oa_location"]:
             pdf_url = data["best_oa_location"]["url_for_pdf"]
-            # print(f"Open access PDF for {doi}: {pdf_url}")
             return pdf_url
     return False
 
@@ -37,11 +35,10 @@ def download_from_doi(doi):
 
 # Function to download PDF
 def download_pdf(pdf_url, filename):
-    response = requests.get(pdf_url)
+    response = requests.get(pdf_url, headers=headers)
     if response.status_code == 200:
         with open(filename, "wb") as f:
             f.write(response.content)
-        # print(f"Downloaded PDF: {filename}")
         return True
     return False
 
--- tests/test_unpaywall.py
+++ tests/test_unpaywall.py
@@ -9,9 +9,20 @@ load_dotenv()
 # Read API keys and other sensitive data from environment variables
 UNPAYWALL_EMAIL = os.getenv("UNPAYWALL_EMAIL")
 
-
-def test_unpaywall():
-    TEST_DOI="10.1109/ACCESS.2019.2931762"
+def test_set_email():
     unpaywall.set_email(UNPAYWALL_EMAIL)
+    assert unpaywall.UNPAYWALL_EMAIL == UNPAYWALL_EMAIL
+
+def test_get_url():
+    TEST_DOI="10.1109/ACCESS.2019.2931762"
     url = unpaywall.get_url(TEST_DOI)
     assert url == "https://ieeexplore.ieee.org/ielx7/6287639/8600701/08779607.pdf"
+
+def test_download_doi():
+    TEST_DOI="10.1109/ACCESS.2019.2931762"
+    result = unpaywall.download_from_doi(TEST_DOI)
+    assert result
+    assert os.path.exists("10.1109_ACCESS.2019.2931762.pdf")
+    os.remove("10.1109_ACCESS.2019.2931762.pdf")
+    assert not os.path.exists("10.1109_ACCESS.2019.2931762.pdf")
+
