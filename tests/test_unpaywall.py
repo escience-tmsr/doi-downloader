abbc98076af99125a8e38727b9631a85bb1398ae
Reggie Cushing <r.cushing@esciencecenter.nl>
2024-11-06 16:19:18 +0100

    mock API requests in unit tests

--- tests/test_unpaywall.py
+++ tests/test_unpaywall.py
@@ -1,28 +1,24 @@
 import doi_downloader.unpaywall as unpaywall
-import pytest
-import os
-from dotenv import load_dotenv
+import responses
 
-# Load environment variables from .env file
-load_dotenv()
 
 # Read API keys and other sensitive data from environment variables
-UNPAYWALL_EMAIL = os.getenv("UNPAYWALL_EMAIL")
 TEST_DOI="10.1007/s10207-021-00566-3"
 TEST_FILE="10.1007_s10207-021-00566-3.pdf"
 
 def test_set_email():
-    unpaywall.set_email(UNPAYWALL_EMAIL)
-    assert unpaywall.UNPAYWALL_EMAIL == UNPAYWALL_EMAIL
+    unpaywall.set_email("test@test.com")
+    assert unpaywall.UNPAYWALL_EMAIL == "test@test.com"
 
+@responses.activate
 def test_get_url():
+    # Mock the response from the Unpaywall API
+    unpaywall_url = unpaywall.UNPAYWALL_API_URL.format(doi=TEST_DOI, email=unpaywall.UNPAYWALL_EMAIL)
+    responses.add(responses.GET, unpaywall_url,
+                  json={"best_oa_location": {"url_for_pdf": "https://link.springer.com/content/pdf/10.1007/s10207-021-00566-3.pdf"}},
+                  status=200)
+
+
     url = unpaywall.get_url(TEST_DOI)
     assert url =='https://link.springer.com/content/pdf/10.1007/s10207-021-00566-3.pdf'
-
-def test_download_doi():
-    result = unpaywall.download_from_doi(TEST_DOI)
-    assert result
-    assert os.path.exists(TEST_FILE)
-    os.remove(TEST_FILE)
-    assert not os.path.exists(TEST_FILE)
 
