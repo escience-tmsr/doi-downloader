03aafd7ad7da0aca9f5361d5eec2484bc43af3b4
Reggie Cushing <r.cushing@esciencecenter.nl>
2024-11-06 15:20:03 +0100

    update test with smaller file to download

--- doi_downloader/__init__.py
+++ doi_downloader/__init__.py
@@ -1,5 +1,5 @@
-from.unpaywall import set_email
-from.unpaywall import get_url
-from.unpaywall import download_from_doi
-from.unpaywall import download_pdf
-
+# from.unpaywall import set_email
+# from.unpaywall import get_url
+# from.unpaywall import download_from_doi
+# from.unpaywall import download_pdf
+#
--- tests/test_unpaywall.py
+++ tests/test_unpaywall.py
@@ -8,21 +8,21 @@ load_dotenv()
 
 # Read API keys and other sensitive data from environment variables
 UNPAYWALL_EMAIL = os.getenv("UNPAYWALL_EMAIL")
+TEST_DOI="10.1007/s10207-021-00566-3"
+TEST_FILE="10.1007_s10207-021-00566-3.pdf"
 
 def test_set_email():
     unpaywall.set_email(UNPAYWALL_EMAIL)
     assert unpaywall.UNPAYWALL_EMAIL == UNPAYWALL_EMAIL
 
 def test_get_url():
-    TEST_DOI="10.1109/ACCESS.2019.2931762"
     url = unpaywall.get_url(TEST_DOI)
-    assert url == "https://ieeexplore.ieee.org/ielx7/6287639/8600701/08779607.pdf"
+    assert url =='https://link.springer.com/content/pdf/10.1007/s10207-021-00566-3.pdf'
 
 def test_download_doi():
-    TEST_DOI="10.1109/ACCESS.2019.2931762"
     result = unpaywall.download_from_doi(TEST_DOI)
     assert result
-    assert os.path.exists("10.1109_ACCESS.2019.2931762.pdf")
-    os.remove("10.1109_ACCESS.2019.2931762.pdf")
-    assert not os.path.exists("10.1109_ACCESS.2019.2931762.pdf")
+    assert os.path.exists(TEST_FILE)
+    os.remove(TEST_FILE)
+    assert not os.path.exists(TEST_FILE)
 
