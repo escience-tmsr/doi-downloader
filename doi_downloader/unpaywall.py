3a9b8e3f8b0780f07d804aa35b8245280dc531f4
Reggie Cushing <r.cushing@esciencecenter.nl>
2024-11-14 13:31:21 +0100

    merge main

--- doi_downloader/unpaywall.py
+++ doi_downloader/unpaywall.py
@@ -1,4 +1,5 @@
 import requests
+import os
 
 # Read API keys and other sensitive data from environment variables
 UNPAYWALL_EMAIL = None
@@ -18,14 +19,28 @@ def get_url(doi):
         raise EnvironmentError("Please make sure email is set using set_email().")
 
     # Make the request to the Unpaywall API
-    response = requests.get(UNPAYWALL_API_URL.format(doi=doi, email=UNPAYWALL_EMAIL), headers=headers)
+    unpaywall_api_url = UNPAYWALL_API_URL.format(doi=doi, email=UNPAYWALL_EMAIL)
+    print(f"Checking {unpaywall_api_url}...")
+    response = requests.get(unpaywall_api_url, headers=headers)
 
     if response.status_code == 200:
         data = response.json()
         if "best_oa_location" in data and data["best_oa_location"]:
             pdf_url = data["best_oa_location"]["url_for_pdf"]
             return pdf_url
+    if response.status_code == 429:
+        raise ValueError("Rate limit.")
+
     return False
+
+def get_urls(dois):
+    if not UNPAYWALL_EMAIL:
+        raise EnvironmentError("Please make sure email is set using set_email().")
+
+    urls = {}
+    for doi in dois:
+        urls[doi] = get_url(doi)
+    return urls
 
 def download_from_doi(doi):
     pdf_url = get_url(doi)
@@ -33,12 +48,26 @@ def download_from_doi(doi):
         return download_pdf(pdf_url, f"{doi.replace('/', '_')}.pdf")
     return False
 
+def download_from_dois(dois):
+    urls = get_urls(dois)
+    files = {}
+    for doi, pdf_url in urls.items():
+        if pdf_url:
+            file_path = download_pdf(pdf_url, f"{doi.replace('/', '_')}.pdf")
+            if file_path:
+                files[doi] = { "doi": doi, "url": pdf_url, "file_path": file_path }
+            else:
+                files[doi] = False
+    return files
+        
+
 # Function to download PDF
-def download_pdf(pdf_url, filename):
+def download_pdf(pdf_url, filename, directory="."):
     response = requests.get(pdf_url, headers=headers)
     if response.status_code == 200:
-        with open(filename, "wb") as f:
+        full_path = os.path.join(directory, filename)
+        with open(full_path, "wb") as f:
             f.write(response.content)
-        return True
+        return full_path
     return False
 
