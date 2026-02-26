eb93c9e5cf91d73c6064fdcbafa83f312d395a62
eriktks <erikt@xs4all.nl>
2026-02-26 18:27:56 +0100

    added tests

--- browser_plugins/default/tests/background.test.js
+++ browser_plugins/default/tests/background.test.js
@@ -84,11 +84,12 @@ test("retrieving attachment", () => {
 });
 
 test("streams PDF data and triggers a download when expectBrowserDownload is false", async () => {
-  const details = { requestId: "req-1" };
   fakeFilter = {
-    disconnect: function onstop_function() {},
-    onstop: function onstop_function() {},
+    disconnect: jest.fn(),
+    onstop: jest.fn(),
+    write: jest.fn(),
   };
+
   global.browser = {
     webRequest: { filterResponseData: jest.fn().mockReturnValue(fakeFilter) },
     downloads: { download: jest.fn().mockResolvedValue(123) }
@@ -98,49 +99,50 @@ test("streams PDF data and triggers a download when expectBrowserDownload is fal
     removeSlashes: (s) => s.replace(/\//g, "_"),
     failCapture: jest.fn(),
   };
-  global.captureSession["expectBrowserDownload"] = false;
-  processIncomingPdfData({ requestId: "req-3" });
+  global.captureSession = {
+    "expectBrowserDownload": false,
+     "doi": "10.1234/foo.bar",
+  }
+  global.URL = {
+    "createObjectURL": jest.fn().mockReturnValue("blob:fake"),
+    "revokeObjectURL": jest.fn()
+  }
 
+  const details = { requestId: "req-1" };
   processIncomingPdfData(details);
 
   expect(browser.webRequest.filterResponseData).toHaveBeenCalledWith("req-1");
 
+  // test fakeFilter
   const chunk1 = new Uint8Array([1, 2, 3]);
   const chunk2 = new Uint8Array([4, 5]);
-
-  // The function we passed into ondata is stored on fakeFilter.ondata
   fakeFilter.ondata({ data: chunk1 });
   fakeFilter.ondata({ data: chunk2 });
-
-  // Now simulate the response finishing
   await fakeFilter.onstop();
 
-  // We should have forwarded both chunks to the browser
   expect(fakeFilter.write).toHaveBeenCalledTimes(2);
   expect(fakeFilter.write).toHaveBeenNthCalledWith(1, chunk1);
   expect(fakeFilter.write).toHaveBeenNthCalledWith(2, chunk2);
-
-  // The filter should be disconnected
   expect(fakeFilter.disconnect).toHaveBeenCalled();
 
-  // A Blob URL should be created
+  // test object creation
   expect(URL.createObjectURL).toHaveBeenCalledTimes(1);
 
-  // And a download should be triggered with the sanitized filename
+  // test download
   expect(browser.downloads.download).toHaveBeenCalledWith({
     url: "blob:fake",
     filename: "10.1234_foo.bar.pdf", // DOI with '/' replaced by '_' + ".pdf"
     saveAs: false,
   });
 
-  // We don't expect failCapture to be called in the success case
+  // test failCapture
   expect(self.failCapture).not.toHaveBeenCalled();
 });
 
 test("setting expectBrowserDownload prevents browser download", async () => {
   fakeFilter = { 
-    disconnect: function onstop_function() {}, 
-    onstop: function onstop_function() {}, 
+    disconnect: jest.fn(), 
+    onstop: jest.fn(), 
   };
   global.browser = {
     webRequest: { filterResponseData: jest.fn().mockReturnValue(fakeFilter) },
@@ -152,8 +154,10 @@ test("setting expectBrowserDownload prevents browser download", async () => {
     failCapture: jest.fn(),
   };
 
-  global.captureSession = { "doi": "10.1234/doi" };
-  global.captureSession["expectBrowserDownload"] = true;
+  global.captureSession = {
+    "doi": "10.1234/doi",
+    "expectBrowserDownload": true
+  };
   processIncomingPdfData({ requestId: "req-2" });
   await fakeFilter.onstop();
   expect(browser.downloads.download).not.toHaveBeenCalled();
@@ -162,4 +166,23 @@ test("setting expectBrowserDownload prevents browser download", async () => {
   processIncomingPdfData({ requestId: "req-3" });
   await fakeFilter.onstop();
   expect(browser.downloads.download).toHaveBeenCalled();
+});
+
+test("start job",  async() => {
+  const doi = "10.1234/foo.bar";
+  const url = "https://doi.org/" + doi;
+  global.self = {
+    sanitizeDOI: (doi) => doi,
+    sendStatus: (text) => null,
+  }
+  global.browser = {
+    tabs: { create: jest.fn() },
+    storage: { local: { set: jest.fn().mockResolvedValue(undefined) } }
+  }
+  browser.tabs.create.mockResolvedValue(doi);
+ 
+  await startJob(doi);
+  const [ returnValue ] = global.browser.storage.local.set.mock.calls[0];
+  expect(returnValue.job.url).toBe(url);
+  expect(browser.tabs.create).toHaveBeenCalledWith({ url });
 });
