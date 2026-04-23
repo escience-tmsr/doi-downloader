3040f6ef425ffdb1952c13c74ce7bff8ad2f07a8
Erik Tjong Kim Sang <erikt@xs4all.nl>
2026-04-23 17:09:43 +0200

    Update tests/test_doiorg.py
    Co-authored-by: Sander van Rijn <svr003@gmail.com>

--- tests/test_doiorg.py
+++ tests/test_doiorg.py
@@ -3,11 +3,10 @@ from unittest.mock import patch
 import responses
 
 TEST_DOI ="10.1007/s10207-021-00566-3"
-DOIORG_URL = "https://doi.org/{doi}"
 
 @responses.activate
 def test_get_url():
-    doiorg_url = DOIORG_URL.format(doi=TEST_DOI)
+    doiorg_url = doiorg.DOIORG_URL.format(doi=TEST_DOI)
 
     with patch.object(doi_downloader.plugins.doiorg.DoiorgPlugin, "fetch_metadata", return_value=doiorg_url):
         instance = doiorg.DoiorgPlugin()
